5513225b2a72dec39bdf5648a6f0c2081043dc01
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2023-08-30 00:11:40 +0700

    header update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
-
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "3Tee on the Internet",
+  description: "Powered by Nextjs",
+};
 
 export default function RootLayout({
   children,
